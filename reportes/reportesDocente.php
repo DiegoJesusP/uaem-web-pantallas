aaf1e0b8213cfcf39ea442ea8c33c6103d2930bf
DiegoJesusP <spartfomi@outlook.com>
2024-06-20 14:11:57 -0600

    arreglo en reporte docente pdf

--- reportes/reportesDocente.php
+++ reportes/reportesDocente.php
@@ -6,14 +6,13 @@ include('./../php/conexion.php');
 class PDF extends FPDF
 {
     // Cabecera de página
-    function Header()
-{
+    function Header(){
     // Background color
     $this->SetFillColor(48, 83, 149); // Blue color in RGB
     $this->Rect(0, 0, $this->w, 30, 'F'); // Draw filled rectangle
     
     // Logo
-    $this->Image('./../assets/img/LogoUAEMBlanco.png', 10, 5, 29);
+    $this->Image('./../assets/img/LogoUAEMBlanco.png', 10, 3, 29);
     
     // Title
     $this->SetFont('Arial', 'B', 13 );
@@ -33,15 +32,20 @@ class PDF extends FPDF
     if (isset($_GET['periodo'])) {
         $periodo = htmlspecialchars($_GET['periodo']);
     } else {
-        $periodo = '';
+        $periodo = 'No esta definido "periodo"';
+    }
+    if (isset($_GET['anio'])) {
+        $anio = htmlspecialchars($_GET['anio']);
+    }else {
+        $periodo = 'No esta definido "anio"';
     }
 
     // Mostrar el valor del período de manera dinámica
     $this->SetFont('Arial', '', 10);
-    $this->Cell(0, -18, 'Periodo: ' . $periodo, 0, 1, 'C');
+    $this->Cell(0, -18, 'Periodo: ' . $periodo. " " .$anio, 0, 1, 'C');
     
-    $this->Ln(15);
-}
+    $this->Ln(18);
+    }
 
 
     // Pie de página
@@ -62,7 +66,7 @@ class PDF extends FPDF
         $this->SetLineWidth(.3);
         $this->SetFont('', 'B');
         // Cabecera
-        $w = array(95, 95); // Ajustamos el ancho de las columnas para dos columnas
+        $w = array(63, 63, 63); // Ajustamos el ancho de las columnas para dos columnas -- 63, 63, 63
         for ($i = 0; $i < count($header); $i++) {
             $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', true);
         }
@@ -81,7 +85,7 @@ class PDF extends FPDF
             $fill = !$fill;
             $row_count++;
             // Añadir un espacio distintivo cada 23 preguntas
-            if ($row_count % 23 == 0) {
+            if ($row_count % 22 == 0) {
                 $this->Cell(array_sum($w), 0, '', 'T');
                 $this->Ln(5); // Espacio de 5 unidades de altura
             }
@@ -89,6 +93,42 @@ class PDF extends FPDF
         // Línea de cierre
         $this->Cell(array_sum($w), 0, '', 'T');
     }
+
+    //
+    function TablaInicio($dataI)
+{
+    // Colores, ancho de línea y fuente en negrita
+    $this->SetFillColor(48, 83, 149); // Color azul en RGB
+    $this->SetTextColor(255);
+    $this->SetDrawColor(48, 83, 149); // Color azul en RGB
+    $this->SetLineWidth(.3);
+    $this->SetFont('', 'B', 10);
+
+    // Restauración de colores y fuentes
+    $this->SetFillColor(224, 235, 255);
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Datos estáticos combinados con datos de la base de datos
+    $this->Cell(63, 6, 'Unidad academica:', 'LR', 0, 'L', false);
+    $this->Cell(126, 6, $dataI['unidad'], 'LR', 0, 'L', false);
+    $this->Ln();
+    $this->Cell(63, 6, 'Datos Generales:', 'LR', 0, 'L', true);
+    $this->Cell(126, 6, 'Indice (Global)', 'LR', 0, 'L', true);
+    $this->Ln();
+    $this->Cell(63, 6, 'Promedio:', 'LR', 0, 'L', false);
+    $this->Cell(126, 6, 'Valor estatico con variable', 'LR', 0, 'L', false);
+    $this->Ln();
+    $this->Cell(63, 6, 'Nombre:', 'LR', 0, 'L', true);
+    $this->Cell(126, 6, $dataI['nombre_docente'], 'LR', 0, 'L', true);
+    $this->Ln();
+    $this->Cell(63, 6, 'Materia:', 'LR', 0, 'L', false);
+    $this->Cell(126, 6, $dataI['materia'], 'LR', 0, 'L', false);
+    $this->Ln();
+
+    // Línea de cierre
+    $this->Cell(189, 0, '', 'T');
+    $this->Ln(1);
+}
 }
 
 // Crear una instancia de conexión
@@ -130,7 +170,23 @@ if (isset($_GET['anio'])) {
 }else {
     $periodo = 'No esta definido "anio"';
 }
+//--
+$consulta = $conn->prepare("SELECT numcontrol, nombre_docente, ap_paterno_docente, ap_materno_docente, unidad, materia  FROM virtuales WHERE numcontrol = :numcontrol");
+// Vincula los parámetros
+$consulta->bindParam(':numcontrol', $numcontrol);
+// Ejecuta la consulta
+$consulta->execute();
+// Obtiene los resultados
+$resultado = $consulta->fetch(PDO::FETCH_ASSOC);
 
+$dataFromDb = [
+    'nombre_docente' => $resultado['nombre_docente'] ?? 'No definido',
+    'ap_paterno_docente' => $resultado['ap_paterno_docente'] ?? 'No definido',
+    'ap_materno_docente' => $resultado['ap_materno_docente'] ?? 'No definido',
+    'unidad' => $resultado['unidad'] ?? 'No definido',
+    'materia' => $resultado['materia'] ?? 'No definido'
+];
+//--
 // Consulta SQL para obtener los datos
 $consulta = $conn->prepare("SELECT * FROM preguntav WHERE numcontrol = :numcontrol AND id_grupo = :id_grupo AND acta_id = :acta_id");
 $consulta->bindParam(':numcontrol', $numcontrol);
@@ -143,9 +199,10 @@ $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
 // Organizar los datos obtenidos en un array
 $data = [];
+$dataS = [];
 foreach ($resultado as $fila) {
-    for ($i = 1; $i <= 23; $i++) {
-        $data[] = ['Pregunta ' . $i, $fila['r' . $i]];
+    for ($i = 1; $i <= 22; $i++) {
+        $dataS[] = ['Pregunta ' . $i, $fila['r' . $i]];
     }
 }
 
@@ -156,9 +213,11 @@ $pdf->AddPage();
 $pdf->SetFont('Arial', '', 12);
 
 // Títulos de las columnas
-$header = ['Pregunta', 'Respuesta'];
+$header = ['Grupos:', 'B', 'Respuesta'];
+//$headerI = ['Prueba', 'Prueba'];
 
 // Imprimir la tabla
+$pdf->TablaInicio($dataFromDb);
 $pdf->FancyTable($header, $data);
 
 $pdf->Output();
