21acb52062a57627147e3539e4987c1b2df7d84d
DiegoJesusP <spartfomi@outlook.com>
2024-06-25 12:48:50 -0600

    mas tablas dinamicas, especificamente de respuestas

--- reportes/reportesDocente.php
+++ reportes/reportesDocente.php
@@ -2,10 +2,10 @@
 require('./../fpdf/fpdf.php');
 include('./../php/conexion.php');
 
-// Clase PDF extendida con funciones personalizadas
+// Clase PDF
 class PDF extends FPDF
 {
-    // Cabecera de página
+    // Cabecera del pdf
     function Header(){
         // Background color
         $this->SetFillColor(48, 83, 149); // Azul
@@ -24,7 +24,7 @@ class PDF extends FPDF
         $this->Rect(0, $this->GetY()+10, $this->w, 13, 'F'); // Rectángulo lleno para subtítulo
         
         // Subtítulo
-        $this->SetFont('Arial', 'B', 10);
+        $this->SetFont('Arial', 'B', 12);
         $this->SetTextColor(0, 0, 0); // Negro
         $this->Cell(0, 29, utf8_decode("Opinión de Estudiantes"), 0, 1, 'C');
         
@@ -48,25 +48,90 @@ class PDF extends FPDF
     }
     
     // Pie de página
-    function Footer()
-    {
+    function Footer(){
         $this->SetY(-15);
         $this->SetFont('Arial', 'I', 8);
         $this->Cell(0, 10, utf8_decode('Página ') . $this->PageNo() . ' / {nb}', 0, 0, 'C');
     }
     
-    // Función para crear la tabla de datos principal y las tablas adicionales
-    function FancyTable($header, $data, $anchoColumnas, $numGrupos) {
+    function TablaInicio($dataI){
         // Colores, ancho de línea y fuente en negrita
+        $this->SetFillColor(48, 83, 149); // Azul
+        $this->SetTextColor(255);
+        $this->SetDrawColor(48, 83, 149); // Azul
+        $this->SetLineWidth(.3);
+        $this->SetFont('', 'B', 10);
+        
+        // Restauración de colores y fuentes
+        $this->SetFillColor(224, 235, 255); // Azul claro
+        $this->SetTextColor(0);
+        $this->SetFont('');
+        
+        // Datos estáticos combinados con datos de la base de datos
+        $this->SetFont('Arial', '', 10);
+        $this->Cell(60, 6, utf8_decode('Unidad académica:'), 'LR', 0, 'L', true);
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(129, 6, utf8_decode($dataI['unidad']), 'LR', 0, 'C', true);
+        $this->Ln();
+
+        $this->SetFont('Arial', '', 10);
+        $this->Cell(60, 6, utf8_decode('Modalidad:'), 'LR', 0, 'L', false);
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(129, 6, utf8_decode('modalidad que se obtiene de la base de datos'), 'LR', 0, 'C', false);
+        $this->Ln();
+        
+        $this->SetFont('Arial', '', 10);
+        $this->Cell(60, 6, utf8_decode('Datos Generales:'), 'LR', 0, 'L', true);
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(129, 6, utf8_decode('Índice (Global)'), 'LR', 0, 'C', true);
+        $this->Ln();
+        
+        $this->SetFont('Arial', '', 10);
+        $this->Cell(60, 6, utf8_decode('Promedio:'), 'LR', 0, 'L', false);
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(129, 6, utf8_decode('Valor estático con variable'), 'LR', 0, 'C', false);
+        $this->Ln();
+        
+        $this->SetFont('Arial', '', 10);
+        $this->Cell(60, 6, utf8_decode('Asesor(a):'), 'LR', 0, 'L', true);
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(129, 6, utf8_decode($dataI['nombre_docente']. " " . $dataI['ap_paterno_docente'] . " " . $dataI['ap_materno_docente']), 'LR', 0, 'C', true);
+        $this->Ln();
+        
+        
+        
+        // Si $dataI['materia'] es un arreglo
+        $aux = 0;
+        foreach ($dataI['materia'] as $materia) {
+            $this->SetFont('Arial', '', 10);
+            if ($aux != 0){
+                $this->Cell(60, 6, '', 'LR', 0, 'L', false);
+            }else {
+                $this->Cell(60, 6, utf8_decode('Curso(s):'), 'LR', 0, 'L', false);
+            }
+            $this->SetFont('Arial', 'B', 10);
+            $this->Cell(129, 6, utf8_decode($materia), 'LR', 0, 'C', false);
+            $this->Ln(); // Salto de línea después de cada materia
+            $aux ++;
+        }
+
+        // Línea de cierre
+        $this->Cell(189, 0, '', 'T');
+        $this->Ln(2);
+    }
+
+    //Crear la tabla de datos principal y las tablas adicionales
+    function FancyTable($header, $data, $anchoColumnas, $numGrupos) {
+        // Colores, ancho de Linea y fuente en negrita
         $this->SetFillColor(255, 255, 255); // Blanco
         $this->SetTextColor(0, 0, 0); // Negro
         $this->SetDrawColor(48, 83, 149); // Azul
         $this->SetLineWidth(.3);
         $this->SetFont('', 'B');
         
-        // Cabecera dinámica
+        // Cabecera dinamica
         foreach ($header as $i => $title) {
-            $this->Cell($anchoColumnas[$i], 7, $title, 1, 0, 'C', true);
+            $this->Cell($anchoColumnas[$i], 7, $title, 1, 0, 'L', true);
         }
         $this->Ln();
         
@@ -102,68 +167,79 @@ class PDF extends FPDF
         foreach ($titulosTablas as $j => $titulo) {
             $this->Cell($anchoColumnas[0], 6, utf8_decode($titulo), 'LR', 0, 'L', $j % 2 == 0);
             for ($k = 1; $k <= $numGrupos; $k++) {
-                $this->Cell($anchoColumnas[$k], 6, '', 'LR', 0, 'L', false); // Celdas vacías para alinear
+                $this->Cell($anchoColumnas[$k], 6, '', 'LR', 0, 'L', $j % 2 == 0); // Celdas vacías grupos
             }
-            $this->Cell($anchoColumnas[$numGrupos + 1], 6, '', 'LR', 0, 'L', false); // Celda vacía para alinear con total
+            $this->Cell($anchoColumnas[$numGrupos + 1], 6, '', 'LR', 0, 'L', $j % 2 == 0); // Celda vacía total
             $this->Ln();
         }
+        $this->Cell(189, 0, '', 'T');
+        $this->Ln(2);
+        //
+    }
+
+    function TablaRespuestas($anchoColumnas, $numGrupos){
+        $this->SetFillColor(225, 238, 218); // Verde
+        $this->SetTextColor(0, 0, 0); // Negro
+        $this->SetDrawColor(0); // verde
+        $this->SetLineWidth(.3);
+        $this->SetFont('', 'B');
+        
+        // Selección de datos
+        // Select * from preguntav where acta_id = :acta_id and id_grupo = :id_grupo;
+        
+        // Configuración de fuente
+        $this->SetFont('Arial', 'B', 10);
+        $this->Cell(189, 6, utf8_decode('Asesor en Línea'), 'LR', 0, 'C', true);
+        $this->Ln();
+        //
+        $this->SetFillColor(217, 217, 217); // gris
+        $this->SetTextColor(0, 0, 0); // Negro
+        $this->SetDrawColor(0); // negro
+        //
+        $titulosTablas = [
+            'Funciones del Asesor en Línea:'
+        ];
+        $cont = 0;
+        $this->SetFont('Arial', 'B', 10);
+        foreach ($titulosTablas as $j => $titulo) {
+            $this->Cell($anchoColumnas[0], 6, utf8_decode($titulo), 'LR', 0, 'L', $j % 2 == 0);
+            for ($k = 1; $k <= $numGrupos; $k++) {
+                $this->Cell($anchoColumnas[$k], 6, '', 'LR', 0, 'L', $j % 2 == 0); // Celdas vacías grupos
+            }
+            $this->Cell($anchoColumnas[$numGrupos + 1], 6, '', 'LR', 0, 'L', $j % 2 == 0); // Celda vacía total
+            $this->Ln();
+        }
+        $this->Cell(189, 0, '', 'T');
+        $this->Ln();
+        //
+        $this->SetFillColor(224, 235, 255); // azul
+        $this->SetTextColor(0, 0, 0); // Negro
+        $this->SetDrawColor(48, 83, 149); // azul
+        $titulosTablas = [
+            'Estudiantes participantes:',
+            'Satisfacción de desempeño:',
+            'Expectativas cubiertas del curso:',
+            'Asesor en Línea (índice Global):',
+            'Diseño del Curso (índice Global):',
+            'nuevo'
+        ];
+        $cont = 0;
+        $this->SetFont('Arial', 'B', 10);
+        foreach ($titulosTablas as $j => $titulo) {
+            $this->Cell($anchoColumnas[0], 6, utf8_decode($titulo), 'LR', 0, 'L', $j % 2 == 0);
+            for ($k = 1; $k <= $numGrupos; $k++) {
+                $this->Cell($anchoColumnas[$k], 6, '', 'LR', 0, 'L', $j % 2 == 0); // Celdas vacías grupos
+            }
+            $this->Cell($anchoColumnas[$numGrupos + 1], 6, '', 'LR', 0, 'L', $j % 2 == 0); // Celda vacía total
+            $this->Ln();
+        }
+        $this->Cell(189, 0, '', 'T');
+        $this->Ln(2);
     }
     
-    // Función para crear la tabla de inicio con datos estáticos
-    function TablaInicio($dataI)
-    {
-        // Colores, ancho de línea y fuente en negrita
-        $this->SetFillColor(48, 83, 149); // Azul
-        $this->SetTextColor(255);
-        $this->SetDrawColor(48, 83, 149); // Azul
-        $this->SetLineWidth(.3);
-        $this->SetFont('', 'B', 10);
-        
-        // Restauración de colores y fuentes
-        $this->SetFillColor(224, 235, 255); // Azul claro
-        $this->SetTextColor(0);
-        $this->SetFont('');
-        
-        // Datos estáticos combinados con datos de la base de datos
-        $this->SetFont('Arial', '', 10);
-        $this->Cell(60, 6, utf8_decode('Unidad académica:'), 'LR', 0, 'L', false);
-        $this->SetFont('Arial', 'B', 10);
-        $this->Cell(129, 6, utf8_decode($dataI['unidad']), 'LR', 0, 'C', false);
-        $this->Ln();
-        
-        $this->SetFont('Arial', '', 10);
-        $this->Cell(60, 6, utf8_decode('Datos Generales:'), 'LR', 0, 'L', true);
-        $this->SetFont('Arial', 'B', 10);
-        $this->Cell(129, 6, utf8_decode('Índice (Global)'), 'LR', 0, 'C', true);
-        $this->Ln();
-        
-        $this->SetFont('Arial', '', 10);
-        $this->Cell(60, 6, utf8_decode('Promedio:'), 'LR', 0, 'L', false);
-        $this->SetFont('Arial', 'B', 10);
-        $this->Cell(129, 6, utf8_decode('Valor estático con variable'), 'LR', 0, 'C', false);
-        $this->Ln();
-        
-        $this->SetFont('Arial', '', 10);
-        $this->Cell(60, 6, utf8_decode('Asesor(a):'), 'LR', 0, 'L', true);
-        $this->SetFont('Arial', 'B', 10);
-        $this->Cell(129, 6, utf8_decode($dataI['nombre_docente']. " " . $dataI['ap_paterno_docente'] . " " . $dataI['ap_materno_docente']), 'LR', 0, 'C', true);
-        $this->Ln();
-        
-        $this->SetFont('Arial', '', 10);
-        $this->Cell(60, 6, utf8_decode('Curso(s):'), 'LR', 0, 'L', false);
-        $this->Ln(); // Salto de línea para empezar en una nueva fila
-        
-        // Si $dataI['materia'] es un arreglo
-        foreach ($dataI['materia'] as $materia) {
-            $this->Cell(60, 6, '', 'LR', 0, 'L', false);
-            $this->Cell(129, 6, utf8_decode($materia), 'LR', 0, 'L', false);
-            $this->Ln(); // Salto de línea después de cada materia
-        }
-
-        // Línea de cierre
-        $this->Cell(189, 0, '', 'T');
-        $this->Ln(1);
-    }
+    // Resto de tu código...
+    
+    
 }
 
 // Crear una instancia de conexión
@@ -271,6 +347,8 @@ $pdf->SetFont('Arial', '', 12);
 
 $pdf->TablaInicio($dataFromDb);
 $pdf->FancyTable($header, $dataS, $anchoColumnas, $numGrupos);
+$pdf->TablaRespuestas($anchoColumnas, $numGrupos);
+
 
 $pdf->Output();
 
